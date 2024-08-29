03d5ca677a1aa77b109243a6e2c3cf7f0c37caf0
kritikas7 <147063801+kritikas7@users.noreply.github.com>
2024-08-29 11:24:29 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -1,17 +1,15 @@
 
-import java.util.Scanner;
+
 public class Demo1 {
 	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-	    System.out.println("enter l and b of rectangle");
-	    int l=sc.nextInt();
-	    int b=sc.nextInt();
-	    if(l>0 &&b>0) {
+		
+	   
+	    int l=2;
+	    int b=5;
+	 
 	    int area=l*b;
 	    System.out.println("area:"+area);
-	    }
-	    else
-	    	System.out.println("l and b should be greater than 0");
+	   
 }
 }
 
