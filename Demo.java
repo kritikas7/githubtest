e303cf100b6f5769515d5c5ef1946b388cbb39af
kritikas7 <147063801+kritikas7@users.noreply.github.com>
2024-08-29 11:48:56 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,6 +3,9 @@
 
 public class Demo {
 	public static void main(String args[]) {
-		system.out.println("FIRST Jenkins");
+		int a=2;
+		int b=3;
+		sum=a+b;
+		System.out.println("sum is "+sum);
 			}
 }
